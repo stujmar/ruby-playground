4e35d6a6627bc55f8972ebfedafc641d9f7b6581
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-04-14 22:35:21 -0500

    [sm] created flares module and made a bunch of methods did a different syntax to set some to private

---
+++ Flare.rb
@@ -0,0 +1,16 @@
+class Flare
+    def launch
+        red_flares
+        blue_flares
+    end
+
+    private
+
+    def red_flares
+        puts "red flares"
+    end
+
+    def blue_flares
+        puts "blue flares"
+    end
+end
