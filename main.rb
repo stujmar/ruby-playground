4c033bffef785af35d693f6adcc3da5bf0f3464a
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-04-14 22:37:10 -0500

    [sm] brought an instance of Flare into main.rb and called it private public methods working fine

--- main.rb
+++ main.rb
@@ -1,7 +1,11 @@
 require "./Spaceship.rb"
+require "./Flare.rb"
 
 puts Spaceship.thruster_count
 rocky_rocket = Spaceship.new
 
 rocky_rocket.alarm_keys("password")
 rocky_rocket.alarm_keys(123)
+
+july_day = Flare.new
+july_day.launch
