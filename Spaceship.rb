2ccd08e6e4ae2c7881d145725bfcabdf32a110c9
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-04-14 22:10:06 -0500

    [sm] made Spaceship module and moved spaceship method to it

---
+++ Spaceship.rb
@@ -0,0 +1,22 @@
+class Spaceship
+    @@thruster_count = 2
+    @fuel_level = 5
+    def self.thruster_count # self. makes this a class method.
+        "the ship's thruster count is #{@@thruster_count}"
+    end
+    def refuel(amount)
+        @fuel_level = @fuel_level.to_i + amount.to_i
+        puts @fuel_level
+    end
+    def alarm()
+        puts "beep beep boop"
+    end
+    private :alarm
+    def alarm_keys(keycode)
+        if keycode == 123
+            alarm()
+        else
+            puts "wrong key code"
+        end
+    end
+end
--- probes.rb
+++ probes.rb
@@ -27,32 +27,9 @@ Addy = AtmosphericProbe.new
 Addy.deploy(10)
 Addy.take_sample
 
-class Spaceship
-    @@thruster_count = 2
-    @fuel_level = 5
-    def self.thruster_count # self. makes this a class method.
-        "the ship's thruster count is #{@@thruster_count}"
-    end
-    def refuel(amount)
-        @fuel_level = @fuel_level.to_i + amount.to_i
-        puts @fuel_level
-    end
-    def alarm()
-        puts "beep beep boop"
-    end
-    private :alarm
-    def alarm_keys(keycode)
-        if keycode == 123
-            alarm()
-        else
-            puts "wrong key code"
-        end
-    end
-end
-
-puts Spaceship.thruster_count
-rocky_rocket = Spaceship.new
+# puts Spaceship.thruster_count
+# rocky_rocket = Spaceship.new
 # puts rocky_rocket.refuel(2)
 # puts rocky_rocket.refuel(11)
-rocky_rocket.alarm_keys("password")
-rocky_rocket.alarm_keys(123)
+# rocky_rocket.alarm_keys("password")
+# rocky_rocket.alarm_keys(123)
