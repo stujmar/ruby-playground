cdb454b11acb9940962f9a60690c38fe237def5b
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-04-16 18:08:30 -0500

    [sm] annotated equations and tested a different version of values that didn't share the same memory address

--- pointing.rb
+++ pointing.rb
@@ -1,7 +1,14 @@
 # a and b point to the same object
 a=b=1
 
+# same object is true
 puts a.equal?(b)
+
+c = 2
+d = 2
+
+# same value is true
+puts c.equal?(d)
 
 class Sibling
     attr_reader :name
@@ -10,6 +17,7 @@ class Sibling
     end
 end
 
+# different instances same name is false
 sibling1 = Sibling.new("Stu")
 sibling2 = Sibling.new("Stu")
 
