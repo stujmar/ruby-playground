cba76840df846d95d5cc3f6d8d741d8924869b66
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-04-15 21:52:20 -0500

    [sm] filled out executable class example everything is running correctly

--- self.rb
+++ self.rb
@@ -2,4 +2,14 @@ def greet(greeting)
     puts greeting + ", captain!"
 end
 
+result = class Vechicle
+    answer = 7 * 6
+    puts "Calculating in class context: " + answer.to_s
+    greet("Good afternoon")
+    answer
+end
+
+puts "The class calculated: " + result.to_s
+puts Vechicle.superclass
+
 greet("Good morning")
