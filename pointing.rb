57222c02c4a9be0c583c919cbe80cfb1791a2a11
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-04-16 18:16:42 -0500

    [sm] added notes to the logs

--- pointing.rb
+++ pointing.rb
@@ -2,12 +2,14 @@
 a=b=1
 
 # same object is true
+puts "comparing the same variable"
 puts a.equal?(b)
 
 c = 2
 d = 2
 
 # same value is true
+puts "comparing the same variable value"
 puts c.equal?(d)
 
 class Sibling
@@ -21,4 +23,8 @@ end
 sibling1 = Sibling.new("Stu")
 sibling2 = Sibling.new("Stu")
 
+puts "comparing objects"
 puts sibling1.equal?(sibling2)
+
+puts "comparing names"
+puts sibling1.name == sibling2.name
