2d9b82327a55594123e738375f70ba1c43445b66
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-04-15 21:40:07 -0500

    [sm] cleaned up formatting fouund missing end got code running with instances of ship and class

--- Spaceship.rb
+++ Spaceship.rb
@@ -3,25 +3,28 @@ class Spaceship
     protected :call_sign # make the call sign private
     @@thruster_count = 2
     @fuel_level = 5
+
     def initialize
-        @call_sign
+        @call_sign = "shipity-do-da"
     end
+
     def self.thruster_count # self. makes this a class method.
         "the ship's thruster count is #{@@thruster_count}"
     end
+
     def refuel(amount)
         @fuel_level = @fuel_level.to_i + amount.to_i #this isn't working
         puts @fuel_level
     end
 
-    def matches(other)
-        call_sign == other.call_sign
-    end
-
+    # def matches(other)
+    #     call_sign == other.call_sign
+    # end
     def alarm()
         puts "beep beep boop"
     end
     private :alarm
+
     def alarm_keys(keycode)
         if keycode == 123
             alarm()
@@ -33,6 +36,12 @@ end
 
 class Cruiser < Spaceship
     def initialize
-        @call_sign
+        @call_sign = "Cruisin' USA"
     end
-def
+end
+
+shippy = Spaceship.new
+cruisy = Cruiser.new
+
+puts [shippy, cruisy]
+puts "hi"
