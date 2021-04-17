cc438554ab3ce8dc0776b408f506bb7f3c6bccf3
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-04-16 21:08:04 -0500

    [sm] conditionally assigned a instance to a new variable

--- ifelse.rb
+++ ifelse.rb
@@ -8,3 +8,15 @@ end
 
 launch(true)
 launch(false)
+
+class Cat
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+end
+
+kitty ||= Cat.new("kitty")
+
+puts kitty.name
