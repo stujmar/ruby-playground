60ed06dcd430fb136eff87b3f88153c1c8f76095
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-04-16 21:18:00 -0500

    [sm] made and tested a switch case

--- ifelse.rb
+++ ifelse.rb
@@ -21,3 +21,21 @@ kitty ||= Cat.new("kitty")
 kitty ||= Cat.new("Carty") # doesn't execuse.
 
 puts kitty.name
+
+# && || and or
+# different levels of precedence
+# in conditional in && || in conditionals
+# and or for flow control in special situations.
+
+def status(codes)
+    case (codes)
+    when "far" then puts "something far away"
+    when "close" then puts "it is closer"
+    when "here" then puts "it is here"
+    end
+end
+
+status("far")
+status("close")
+status("here")
+
