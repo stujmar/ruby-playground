0ab66adf05ed41c427c77929c11143008cc1b35a
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-04-18 20:27:03 -0500

    [sm] raise keywork in exceptions

--- exceptions.rb
+++ exceptions.rb
@@ -1,3 +1,5 @@
+# this module will not run it is pseudocode
+
 def launch
     begin
         batten_hatches
@@ -19,5 +21,12 @@ rescue LightError
 rescue StandardError => e # let's check what kind of exception it was
     puts e.message
     false
+end
+
+# raise and rescue keywords.
+
+rescue HatchError => err
+    puts $!.message # maybe look this up.
+    raise
 end
 
