e6f6f8818a8cce1413e937ff0e1b20cb284ac91a
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-04-18 20:33:34 -0500

    [sm] wtire some example methods using retry keyword

--- exceptions.rb
+++ exceptions.rb
@@ -30,3 +30,22 @@ rescue HatchError => err
     raise
 end
 
+# ensure claus 
+# ensure some code runs after an exception is caught? exception goes off ensure file is closed or data is cleaned up.
+
+# retrying 
+def batten_hatches
+    hatch_list = API.request("/hatches")
+    # ...
+rescue RuntimeError => e
+    attempts ||= 0
+    attempts += 1
+    if attempts < 3
+        puts e.message + ". Retrying request."
+        retry
+    else
+        puts "Request failed"
+        raise
+    end
+end
+
