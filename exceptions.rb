7a189eef3139c0d9dd43f0094f97ea130ee5cb85
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-04-18 19:32:54 -0500

    [sm] played with some different ways to structure methods to catch exceptions

---
+++ exceptions.rb
@@ -0,0 +1,23 @@
+def launch
+    begin
+        batten_hatches
+    rescue
+        puts "hatches failed"
+        return false # explict return statement required here.
+    end
+    puts "light seatbelt sign" # this could also raise an expection.
+end
+
+# exception catching at end catches any exceptions.
+def launch
+    batten_hatches
+    light_seatbelt_sign
+    true
+rescue LightError
+    puts "Lights not working, stil launching"
+    true
+rescue StandardError => e # let's check what kind of exception it was
+    puts e.message
+    false
+end
+
